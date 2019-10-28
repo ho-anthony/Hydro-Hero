e453e5f6e28689ff42a661960127e10b8c8fc8a5
katkell <katkellx@gmail.com>
2019-10-27 20:26:08 -0700

    created fragments and connected to toolbar/fragment display

--- app/src/main/java/com/example/hydrohomie/activities/MainActivity.java
+++ app/src/main/java/com/example/hydrohomie/activities/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.hydrohomie.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.hydrohomie.R;
+import com.example.hydrohomie.fragments.HistoryFragment;
+import com.example.hydrohomie.fragments.HomeFragment;
+import com.example.hydrohomie.fragments.SettingsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +20,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //sets default fragment to home fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_display,
+                new HomeFragment()).commit();
+
     }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    //switches created fragment based on user select
+                    switch(menuItem.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_history:
+                            selectedFragment = new HistoryFragment();
+                            break;
+                        case R.id.nav_settings:
+                            selectedFragment = new SettingsFragment();
+                            break;
+
+                    }
+
+                    //displays selected fragment
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_display,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
