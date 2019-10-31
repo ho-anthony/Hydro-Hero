e222ef6ff3ae9a238ba78505d2b0c60a0f26e24f
Anthony Ho <ankho@ucsc.edu>
2019-10-30 19:52:52 -0700

    onclick setup

--- app/src/main/java/com/example/hydrohomie/fragments/HomeFragment.java
+++ app/src/main/java/com/example/hydrohomie/fragments/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,10 +14,29 @@ import androidx.fragment.app.Fragment;
 import com.example.hydrohomie.R;
 
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
+
+    Button addButton;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_home, container,false );
+        addButton = (Button) rootView.findViewById(R.id.addButton);
+        addButton.setOnClickListener(this);
+        return rootView;
+    }
+
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.addButton:
+                test(v);
+                break;
+        }
+
+    }
+
+    public void test(View v) {
+        Toast.makeText(getActivity(), "working!", Toast.LENGTH_SHORT).show();
     }
 }
