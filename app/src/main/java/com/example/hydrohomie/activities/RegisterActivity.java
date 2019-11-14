9f8073dd08c36968f24c16e9ccbe9f01599a939f
Anthony Ho <ankho@ucsc.edu>
2019-11-14 01:26:42 -0800

    Account creation working

--- app/src/main/java/com/example/hydrohomie/activities/RegisterActivity.java
+++ app/src/main/java/com/example/hydrohomie/activities/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.hydrohomie.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -11,11 +12,16 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.hydrohomie.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
     EditText name, email, password, confirmPass;
     Button submit;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,7 @@ public class RegisterActivity extends AppCompatActivity {
         password = findViewById(R.id.passwordInput);
         confirmPass = findViewById(R.id.confirmInput);
         submit = findViewById(R.id.registerButton);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void submitInfo(View v) {
@@ -33,12 +40,21 @@ public class RegisterActivity extends AppCompatActivity {
             Toast.makeText(this, "Name is required", Toast.LENGTH_SHORT).show();
         } else if(email.getText().toString().trim().equals("") || !isValidEmail(email.getText())) {
             Toast.makeText(this, "Email is required/invalid", Toast.LENGTH_SHORT).show();
-        } else if(password.getText().toString().length() < 6) {
+        } else if(password.getText().toString().trim().length() < 6) {
             Toast.makeText(this, "Password must be at least 6 characters long", Toast.LENGTH_SHORT).show();
         } else if(!password.getText().toString().equals(confirmPass.getText().toString())) {
             Toast.makeText(this, "Password does not match", Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(this, "Account Created", Toast.LENGTH_SHORT).show();
+            mAuth.createUserWithEmailAndPassword(email.getText().toString().trim(), password.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()) {
+                        Toast.makeText(getApplicationContext(),"Account Created",Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(),"failed :(",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         }
     }
 
