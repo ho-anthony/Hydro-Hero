23cce67b77393b00c0426ddabf2c86b945c0e965
Anthony Ho <ankho@ucsc.edu>
2019-11-13 22:53:54 -0800

    Registration UI

--- app/src/main/java/com/example/hydrohomie/activities/LoginActivity.java
+++ app/src/main/java/com/example/hydrohomie/activities/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.hydrohomie.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.hydrohomie.R;
 
@@ -13,4 +15,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void onSignUp(View v) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/hydrohomie/activities/RegisterActivity.java
+++ app/src/main/java/com/example/hydrohomie/activities/RegisterActivity.java
@@ -1,4 +1,16 @@
 package com.example.hydrohomie.activities;
 
-public class RegisterActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hydrohomie.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
 }
