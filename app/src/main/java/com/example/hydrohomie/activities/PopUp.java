0d620e11374dcfa0da19a3351c0e9dcf94ede892
katkell <katkellx@gmail.com>
2019-11-25 03:14:32 -0800

    added method to get input from radiobuttons + updated popup window UI

--- app/src/main/java/com/example/hydrohomie/activities/PopUp.java
+++ app/src/main/java/com/example/hydrohomie/activities/PopUp.java
@@ -2,11 +2,24 @@ package com.example.hydrohomie.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.util.DisplayMetrics;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.hydrohomie.R;
 
 public class PopUp extends Activity {
+
+    private RadioGroup drinkOptionGroup;
+    private RadioButton radioDrink;
+    private Button selectButton;
+    //user input (fix)
+    private int finalValue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -21,6 +34,32 @@ public class PopUp extends Activity {
         int height =dm.heightPixels;
 
         getWindow().setLayout((int)(width*.8),(int)(height*.6));
+
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton(){
+        drinkOptionGroup = findViewById(R.id.drinkOptions);
+        selectButton = findViewById(R.id.addDrinkButton);
+
+        selectButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //gets selected radioButton from group
+                int selectedID = drinkOptionGroup.getCheckedRadioButtonId();
+                radioDrink = findViewById(selectedID);
+
+                //print amount added
+                Toast.makeText(PopUp.this,
+                        radioDrink.getText() + " added!", Toast.LENGTH_SHORT).show();
+
+                finish();
+                //IMPORTANT: if no button selected and "add drink clicked", app crashes (fix)
+            }
+        });
+
+
+
     }
 
 }
