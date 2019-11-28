3316353969415999646ae03cf73917328b340b24
Alan <ali59@ucsc.edu>
2019-11-28 14:01:50 -0800

    can open new profile activity

--- app/src/main/java/com/example/hydrohomie/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/hydrohomie/fragments/SettingsFragment.java
@@ -1,21 +1,43 @@
 package com.example.hydrohomie.fragments;
 
+import android.content.Intent;
+import android.location.SettingInjectorService;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.hydrohomie.R;
+import com.example.hydrohomie.activities.ProfileActivity;
 
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements View.OnClickListener {
+
+    private Button openProfileButton;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_settings, container,false );
+        openProfileButton = (Button) rootView.findViewById(R.id.profile);
+        openProfileButton.setOnClickListener(this);
+        return rootView;
+    }
+
+
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.profile:
+                Intent myIntent = new Intent(SettingsFragment.this.getActivity(), ProfileActivity.class);
+                startActivity(myIntent);
+                break;
+
+
+        }
+
     }
 }
