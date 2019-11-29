7c12c22d1feef114aca9a68f9a176221ccaf53ce
Alan <ali59@ucsc.edu>
2019-11-28 18:03:21 -0800

    should be able to make shared preferences file

--- app/src/main/java/com/example/hydrohomie/activities/ProfileActivity.java
+++ app/src/main/java/com/example/hydrohomie/activities/ProfileActivity.java
@@ -2,15 +2,67 @@ package com.example.hydrohomie.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.hydrohomie.R;
 
 public class ProfileActivity extends AppCompatActivity {
+    private Button saveButton;
+    private EditText editName;
+    private EditText editAge;
+    private EditText editWeight;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String NAME = "text";
+    public static final String AGE = "text";
+    public static final String WEIGHT = "text";
+
+    public String savedName;
+    public Integer savedAge;
+    public Integer savedWeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        editName = (EditText) findViewById(R.id.nameEntry);
+        editAge = (EditText) findViewById(R.id.ageEntry);
+        editWeight = (EditText) findViewById(R.id.weightEntry);
+        saveButton = (Button) findViewById(R.id.save);
+
+
+
+        saveButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                saveData();
+            }
+        });
+
+        loadData();
     }
+
+    public void saveData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(NAME,editName.getText().toString());
+        editor.putInt(AGE,Integer.parseInt(editAge.getText().toString()));
+        editor.putInt(WEIGHT,Integer.parseInt(editWeight.getText().toString()));
+        editor.apply();
+
+    }
+
+    public void loadData(){
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
+        savedName = sharedPreferences.getString(NAME,"");
+        savedAge = sharedPreferences.getInt(AGE,0);
+        savedWeight = sharedPreferences.getInt(WEIGHT,0);
+    }
+
+
 }
