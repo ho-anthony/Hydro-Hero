fe571834229b8ad5dc2e9ba142d07a74966bdee2
katkell <katkellx@gmail.com>
2019-11-30 04:45:32 -0800

    fixed pop up to get correct amount of water from user
    improved pop up (now checks for null input from user, correctly gets default and custom amounts of water from the user, customized print messages)

--- app/src/main/java/com/example/hydrohomie/activities/PopUp.java
+++ app/src/main/java/com/example/hydrohomie/activities/PopUp.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.util.DisplayMetrics;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -18,7 +19,7 @@ public class PopUp extends Activity {
     private RadioButton radioDrink;
     private Button selectButton;
     //user input (fix)
-    private int finalValue;
+    public int finalAmount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -26,40 +27,72 @@ public class PopUp extends Activity {
 
         setContentView(R.layout.drink_select_popup);
 
-        //set size of pop up window to match phone dimensions
+        //set size of pop up window to change based on phone dimensions
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels; //get width of phone screen
+        int height = dm.heightPixels; //get height of phone screen
+        getWindow().setLayout((int)(width*.8),(int)(height*.5)); //set pop up size
 
-        int width = dm.widthPixels;
-        int height =dm.heightPixels;
-
-        getWindow().setLayout((int)(width*.8),(int)(height*.5));
-
-        addListenerOnButton();
+        confirmFinalAmount();
     }
 
-    public void addListenerOnButton(){
-        drinkOptionGroup = findViewById(R.id.drinkOptions);
+    //method that changes finalAmount value based on user radio button selection for default options
+    public int findFinalAmount(View view){
+        boolean checked = ((RadioButton) view).isChecked();
+        //find which option was selected
+        switch(view.getId()){
+            case R.id.option1:
+                if(checked){
+                    finalAmount = 8;
+                }
+                break;
+            case R.id.option2:
+                if(checked){
+                    finalAmount = 12;
+                }
+                break;
+            case R.id.option3:
+                if(checked){
+                    finalAmount = 16;
+                }
+                break;
+        }
+        return finalAmount;
+    }
+
+    public void confirmFinalAmount(){
         selectButton = findViewById(R.id.addDrinkButton);
 
         selectButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                //gets selected radioButton from group
-                int selectedID = drinkOptionGroup.getCheckedRadioButtonId();
-                radioDrink = findViewById(selectedID);
+                //checks for custom amount if used
+                RadioButton custom;
+                custom = findViewById(R.id.optionCustom);
+                if(custom.isChecked()){
+                    //creates custom amount from editText
+                    int customVal = 0;
+                    EditText amt = findViewById(R.id.customAmount);
+                    String temp = amt.getText().toString();
+                    if (!"".equals(temp)){
+                        customVal = Integer.parseInt(temp);
+                    }
+                    finalAmount = customVal;
+                }
 
                 //print amount added
-                Toast.makeText(PopUp.this,
-                        radioDrink.getText() + " added!", Toast.LENGTH_SHORT).show();
-
+                if(finalAmount == 0){
+                    Toast.makeText(PopUp.this,
+                            "Stay hydrated!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(PopUp.this, "You drank " + finalAmount +
+                            " oz. added, nice!", Toast.LENGTH_SHORT).show();
+                }
                 finish();
-                //IMPORTANT: if no button selected and "add drink clicked", app crashes (fix)
             }
         });
-
-
-
     }
 
 }
