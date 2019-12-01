1035b6453e4b0739aaa92c221d061decbbf17eaa
Alan <ali59@ucsc.edu>
2019-12-01 01:02:14 -0800

    shared preferences working, recommendations working, display logged amount working, profile page working and saving user info correctly

--- app/src/main/java/com/example/hydrohomie/activities/ProfileActivity.java
+++ app/src/main/java/com/example/hydrohomie/activities/ProfileActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.hydrohomie.R;
 
@@ -17,9 +18,9 @@ public class ProfileActivity extends AppCompatActivity {
     private EditText editWeight;
 
     public static final String SHARED_PREFS = "sharedPrefs";
-    public static final String NAME = "text";
-    public static final String AGE = "text";
-    public static final String WEIGHT = "text";
+    public static final String NAME = "name";
+    public static final String AGE = "age";
+    public static final String WEIGHT = "weight";
 
     public String savedName;
     public Integer savedAge;
@@ -27,9 +28,10 @@ public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
+        loadData();
         editName = (EditText) findViewById(R.id.nameEntry);
         editAge = (EditText) findViewById(R.id.ageEntry);
         editWeight = (EditText) findViewById(R.id.weightEntry);
@@ -44,24 +46,29 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
-        loadData();
+
     }
 
     public void saveData(){
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(NAME,editName.getText().toString());
-        editor.putInt(AGE,Integer.parseInt(editAge.getText().toString()));
-        editor.putInt(WEIGHT,Integer.parseInt(editWeight.getText().toString()));
-        editor.apply();
-
+        if(editName.getText().toString().trim().equals("")||editAge.getText().toString().trim().equals("")
+                ||editWeight.getText().toString().trim().equals("")){
+            Toast.makeText(this,"Invalid Input",Toast.LENGTH_SHORT).show();
+        }else {
+            SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(NAME, editName.getText().toString());
+            editor.putInt(AGE, Integer.parseInt(editAge.getText().toString()));
+            editor.putInt(WEIGHT, Integer.parseInt(editWeight.getText().toString()));
+            editor.apply();
+            finish();
+        }
     }
 
     public void loadData(){
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
         savedName = sharedPreferences.getString(NAME,"");
-        savedAge = sharedPreferences.getInt(AGE,0);
-        savedWeight = sharedPreferences.getInt(WEIGHT,0);
+        savedAge = sharedPreferences.getInt(AGE,18);
+        savedWeight = sharedPreferences.getInt(WEIGHT,90);
     }
 
 
--- app/src/main/java/com/example/hydrohomie/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/hydrohomie/fragments/SettingsFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,12 +20,17 @@ import com.example.hydrohomie.activities.ProfileActivity;
 public class SettingsFragment extends Fragment implements View.OnClickListener {
 
     private Button openProfileButton;
+    private Button aboutUsButton;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_settings, container,false );
         openProfileButton = (Button) rootView.findViewById(R.id.profile);
         openProfileButton.setOnClickListener(this);
+
+        aboutUsButton = (Button) rootView.findViewById(R.id.aboutUs);
+        aboutUsButton.setOnClickListener(this);
         return rootView;
     }
 
@@ -34,6 +40,12 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
             case R.id.profile:
                 Intent myIntent = new Intent(SettingsFragment.this.getActivity(), ProfileActivity.class);
                 startActivity(myIntent);
+                break;
+            case R.id.aboutUs:
+                Toast.makeText(getContext(),"We, the hydro homies, are a group of students " +
+                        "working to develop this app to help people stay healthy and hydrated! " +
+                        "By doing this, we hope to give a helping hand to the people out there wh" +
+                        "o need a reason or reminder to stay healthy.",Toast.LENGTH_LONG).show();
                 break;
 
 
